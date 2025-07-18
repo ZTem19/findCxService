b0bfdc4b287c4170a7f1e03933f181d68da503b0
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 <zander.templin@gmail.com>
2025-07-18 15:32:44 -0500

    backing up

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="flex h-screen items-center justify-center bg-gray-900 text-white">
-        <h1 className="text-4xl font-bold">Tailwind is Working! 🎉</h1>
+      <div>
+        <h1> Yippie</h1>
       </div>
     </>
   );
